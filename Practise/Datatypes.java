c2f1d012e8c2aa4a89bc288dc018f2460dd9572a
Preethika-10 <vpreethika10@gmail.com>
2026-01-06 21:37:52 +0530

    Add files via upload

---
+++ Practise/Datatypes.java
@@ -0,0 +1,20 @@
+public class Datatypes {
+       public static void main(String[] args){
+             int num = 100;
+             System.out.println("The number is "+num);
+             byte sqr = 81;
+             System.out.println("What is the square of 9? "+sqr);
+             float pi = 3.14f;
+             System.out.println("The value of pi is "+pi);
+             double gr = 1.61803d;
+             System.out.println("The Golden ratio is "+gr);
+             short days = 365;
+             System.out.println("The number of days in a standard,non-leap year is "+days);
+             long min = 525600L;
+             System.out.println("No.of minutes in a year is "+min);
+             char grade = 'O';
+             System.out.println("The top grade is "+grade);
+             boolean value = true;
+             System.out.println("Is the number 1 neither prime nor composite? "+value);
+       }
+}
